dd0d8a6245d00e9684d40ad0ae752e92bc77e731
Yi Qu yqu <yqu@ip-10-113-132-29.evoforge.org>
2022-09-26 17:44:18 +0000

    fb

--- src/App.js
+++ src/App.js
@@ -2,6 +2,14 @@ import React from 'react';
 import Core from './core/Core';
 import './App.scss';
 import { AppThemeContextProvider } from './store/context/theme-context';
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  //...
+};
+
+// eslint-disable-next-line no-unused-vars
+const app = initializeApp(firebaseConfig);
 
 function App() {
   return (
